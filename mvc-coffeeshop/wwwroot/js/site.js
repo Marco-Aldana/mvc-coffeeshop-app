ddcbf56627bddb5224bdbcdc6165a59fa34eaf29
marcoantonioaldanadiaz <marco.aldana.d@outlook.com>
2023-04-01 13:48:20 -0600

    .

--- mvc-coffeeshop/wwwroot/js/site.js
+++ mvc-coffeeshop/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function myNav() {
+    let bar = document.querySelector(".bar");
+    let nav = document.querySelector(".navigation");
+    bar.onclick = () => {
+        if (nav.style.left == "0%") {
+            nav.style.left = "-100%";
+            bar.src = "pic/menu.png"
+        } else {
+            nav.style.left = "0%";
+            bar.src = "pic/x.png"
+        }
+    }
+}
+myNav();
+
+window.onload = () => {
+    let homeContent = document.querySelector(".home-content");
+    homeContent.style.left = "50px"
+}
+
+
+
+
